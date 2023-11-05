9d16f3c5abfe2d7141025c1fa4179eb4deb03ad3
Oumaymatouil <oumayma.touil@esprit.tn>
2023-11-05 18:15:55 +0100

    test unitaire

--- src/test/java/tn/esprit/spring/khaddem/ContratServiceTest.java
+++ src/test/java/tn/esprit/spring/khaddem/ContratServiceTest.java
@@ -17,8 +17,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -47,6 +46,72 @@ public class ContratServiceTest {
         Contrat contrat1 = contratService.retrieveContrat(1);
         assertNotNull(contrat1);
         verify(contratRepository).findById(Mockito.anyInt());
+    }
+
+    @Test
+    public void retrieveAllContratsTest(){
+        Mockito.when(contratRepository.findAll()).thenReturn(list);
+        List<Contrat> contrats = contratService.retrieveAllContrats();
+        assertNotNull(contrats);
+        verify(contratRepository).findAll();
+    }
+
+    @Test
+    public void updateContratTest() {
+        // Create a sample Contrat to be updated
+        Contrat updatedContrat = Contrat.builder()
+                .idContrat(1)
+                .archived(true)
+                .montantContrat(1000)  // Updated montantContrat value
+                .dateDebutContrat(new Date())
+                .dateFinContrat(new Date())
+                .specialite(Specialite.RESEAU)
+                .build();
+
+        // Mock the repository's save method
+        Mockito.when(contratRepository.save(updatedContrat)).thenReturn(updatedContrat);
+
+        // Call the updateContrat method
+        Contrat updated = contratService.updateContrat(updatedContrat);
+
+        assertNotNull(updated);
+        assertEquals(1000, updated.getMontantContrat());
+        verify(contratRepository).save(updatedContrat);
+    }
+
+    @Test
+    public void removeContratTest() {
+        int contratId = 1; // ID of the Contrat to be removed
+
+        // Mock the repository's deleteById method
+        Mockito.doNothing().when(contratRepository).deleteById(contratId);
+
+        // Call the removeContrat method
+        contratService.removeContrat(contratId);
+
+        verify(contratRepository, times(1)).deleteById(contratId);
+    }
+
+    @Test
+    public void addContratTest() {
+        Contrat newContrat = Contrat.builder()
+                .idContrat(4)  // A new ID
+                .archived(false) // Example values
+                .montantContrat(1500)
+                .dateDebutContrat(new Date())
+                .dateFinContrat(new Date())
+                .specialite(Specialite.IA)
+                .build();
+
+        // Mock the repository's save method
+        Mockito.when(contratRepository.save(newContrat)).thenReturn(newContrat);
+
+        // Call the addContrat method
+        Contrat addedContrat = contratService.addContrat(newContrat);
+
+        assertNotNull(addedContrat);
+        assertEquals(4, addedContrat.getIdContrat()); // Check if the ID is set correctly
+        verify(contratRepository).save(newContrat);
     }
 
 
