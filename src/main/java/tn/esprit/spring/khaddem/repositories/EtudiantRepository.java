ce02d795a768697a07838a7937bd83426e69e64d
sofienbensalem <60828973+sofienbensalem@users.noreply.github.com>
2023-11-05 22:54:27 +0100

    Etudiant Update

--- src/main/java/tn/esprit/spring/khaddem/repositories/EtudiantRepository.java
+++ src/main/java/tn/esprit/spring/khaddem/repositories/EtudiantRepository.java
@@ -4,18 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-//import tn.esprit.spring.khaddem.entities.Contrat;
+import tn.esprit.spring.khaddem.entities.Contrat;
 import tn.esprit.spring.khaddem.entities.Etudiant;
-//import tn.esprit.spring.khaddem.entities.Niveau;
-//import tn.esprit.spring.khaddem.entities.Specialite;
+import tn.esprit.spring.khaddem.entities.Niveau;
+import tn.esprit.spring.khaddem.entities.Specialite;
 
 import java.util.List;
 
 @Repository
 public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
 
-    //public List<Etudiant> findByDepartementIdDepartement(Long idDepartement);
-    //public List<Etudiant> findByEquipesNiveau(Niveau nv);
+    public List<Etudiant> findByDepartementIdDepartement(Long idDepartement);
+    public List<Etudiant> findByEquipesNiveau(Niveau nv);
 
 
 
@@ -34,9 +34,9 @@ public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
 
 
 
-  //  List<Etudiant> findByDepartementIdDepartement(Integer idDepartement);
+    List<Etudiant> findByDepartementIdDepartement(Integer idDepartement);
 
-//   List<Etudiant> findByEquipesNiveau(Niveau niveau);
+   List<Etudiant> findByEquipesNiveau(Niveau niveau);
 
    Etudiant findByNomEAndPrenomE(String nomE, String prenomE);
 
@@ -45,17 +45,17 @@ public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
 
 
 
-//    @Query("SELECT e FROM Etudiant e, Contrat c where "
-//           + "e.idEtudiant = c.etudiant.idEtudiant "
-//           + "and c.specialite =:specialite ")
-//   List<Etudiant> retrieveEtudiantsByContratSpecialite(@Param("specialite") Specialite specialite);
+    @Query("SELECT e FROM Etudiant e, Contrat c where "
+           + "e.idEtudiant = c.etudiant.idEtudiant "
+           + "and c.specialite =:specialite ")
+   List<Etudiant> retrieveEtudiantsByContratSpecialite(@Param("specialite") Specialite specialite);
 
- //  @Query(value = "SELECT * FROM etudiant e INNER JOIN contrat c ON e.id_etudiant =   c.etudiant_id_etudiant where c.specialite = :specialite", nativeQuery = true)
-  // List<Etudiant> retrieveEtudiantsByContratSpecialiteSQL(@Param("specialite") Specialite specialite);
+   @Query(value = "SELECT * FROM etudiant e INNER JOIN contrat c ON e.id_etudiant =   c.etudiant_id_etudiant where c.specialite = :specialite", nativeQuery = true)
+   List<Etudiant> retrieveEtudiantsByContratSpecialiteSQL(@Param("specialite") Specialite specialite);
 
-// @Query(value = "SELECT * FROM etudiant e INNER JOIN contrat c ON e.id_etudiant =   c.etudiant_id_etudiant where c.specialite =:specialite", nativeQuery = true)
-// List<Etudiant> retrieveEtudiantsByContratSpecialiteSQL(@Param("specialite") String specialite);
-//
+ @Query(value = "SELECT * FROM etudiant e INNER JOIN contrat c ON e.id_etudiant =   c.etudiant_id_etudiant where c.specialite =:specialite", nativeQuery = true)
+ List<Etudiant> retrieveEtudiantsByContratSpecialiteSQL(@Param("specialite") String specialite);
+
 
 
 
