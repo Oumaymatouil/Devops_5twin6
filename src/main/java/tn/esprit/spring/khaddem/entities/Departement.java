7306998f512510a303739af0dbae818ee4b7c7be
lamis11 <61513951+lamis11@users.noreply.github.com>
2023-11-05 23:00:53 +0100

    Departement updating implementation

--- src/main/java/tn/esprit/spring/khaddem/entities/Departement.java
+++ src/main/java/tn/esprit/spring/khaddem/entities/Departement.java
@@ -21,8 +21,8 @@ public class Departement implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idDepartement;
     private String nomDepart;
-    //@OneToMany(mappedBy = "departement")
-    //@JsonIgnore
-    //private List<Etudiant> etudiants;
+    @OneToMany(mappedBy = "departement")
+    @JsonIgnore
+    private List<Etudiant> etudiants;
 
 }
--- src/main/java/tn/esprit/spring/khaddem/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/khaddem/services/DepartementServiceImpl.java
@@ -3,9 +3,9 @@ package tn.esprit.spring.khaddem.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.spring.khaddem.entities.Departement;
-//import tn.esprit.spring.khaddem.entities.Universite;
+import tn.esprit.spring.khaddem.entities.Universite;
 import tn.esprit.spring.khaddem.repositories.DepartementRepository;
-//import tn.esprit.spring.khaddem.repositories.UniversiteRepository;
+import tn.esprit.spring.khaddem.repositories.UniversiteRepository;
 
 import java.util.List;
 
@@ -14,7 +14,7 @@ public class DepartementServiceImpl implements IDepartementService{
     @Autowired
     DepartementRepository departementRepository;
     @Autowired
-    //UniversiteRepository universiteRepository;
+    UniversiteRepository universiteRepository;
     @Override
     public List<Departement> retrieveAllDepartements() {
         return departementRepository.findAll();
@@ -37,9 +37,9 @@ public class DepartementServiceImpl implements IDepartementService{
         return departementRepository.findById(idDepart).get();
     }
 
-    //@Override
-    //public List<Departement> retrieveDepartementsByUniversite(Integer idUniversite) {
-       // Universite universite = universiteRepository.findById(idUniversite).get();
-       // return universite.getDepartements();
-    //}
+    @Override
+    public List<Departement> retrieveDepartementsByUniversite(Integer idUniversite) {
+        Universite universite = universiteRepository.findById(idUniversite).get();
+        return universite.getDepartements();
+    }
 }
