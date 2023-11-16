4f4be5b7bb6015bf3c8e82098a89d297a5af7f2d
sofienbensalem <60828973+sofienbensalem@users.noreply.github.com>
2023-11-16 02:12:26 +0100

    test unitaire biko

--- src/main/java/tn/esprit/spring/khaddem/entities/Etudiant.java
+++ src/main/java/tn/esprit/spring/khaddem/entities/Etudiant.java
@@ -16,7 +16,7 @@ public class Etudiant  implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idEtudiant;
     private String prenomE;
     private String nomE;
--- src/test/java/tn/esprit/spring/khaddem/services/EtudiantServiceImplTest.java
+++ src/test/java/tn/esprit/spring/khaddem/services/EtudiantServiceImplTest.java
@@ -1,41 +1,57 @@
 package tn.esprit.spring.khaddem.services;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tn.esprit.spring.khaddem.KhaddemApplication;
 import tn.esprit.spring.khaddem.entities.*;
 import tn.esprit.spring.khaddem.repositories.ContratRepository;
+import tn.esprit.spring.khaddem.repositories.DepartementRepository;
 import tn.esprit.spring.khaddem.repositories.EquipeRepository;
 import tn.esprit.spring.khaddem.repositories.EtudiantRepository;
+import tn.esprit.spring.khaddem.services.EtudiantServiceImpl;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class EtudiantServiceImplTest {
+
+    @Mock
     @Autowired
     private EquipeRepository equipeRepository;
+    @Mock
     @Autowired
     private ContratRepository contratRepository;
+    @Autowired
     @Mock
     EtudiantRepository etudiantRepository;
+    @Autowired
+    @Mock
+    DepartementRepository departementRepository;
     @InjectMocks
     EtudiantServiceImpl etudiantService;
+    @InjectMocks
+    DepartementServiceImpl departementService;
+    @InjectMocks
+    ContratServiceImpl contratService;
+    @InjectMocks
+    EquipeServiceImpl equipeService;
 
   Etudiant e = new Etudiant(1,"sofien","Bensalem", Option.GAMIX,new Departement(),new ArrayList<Equipe>(),new ArrayList<Contrat>());
+
 
     @Test
     void retrieveAllEtudiants() {
@@ -43,9 +59,7 @@ class EtudiantServiceImplTest {
         Assertions.assertNotNull(etudiantList);
     }
 
-    @Test
-    void addEtudiant() {
-    }
+
 
     @Test
     void updateEtudiant() {
@@ -54,9 +68,7 @@ class EtudiantServiceImplTest {
         verify(etudiantRepository, times(1)).save(e);
     }
 
-    @Test
-    void retrieveEtudiant() {
-    }
+
 
     @Test
     void removeEtudiant() {
@@ -66,30 +78,132 @@ class EtudiantServiceImplTest {
     }
 
     @Test
-    void assignEtudiantToDepartement() {
+    public void testAssignEtudiantToDepartement() {
+        List<Etudiant> list= new ArrayList<Etudiant>() {
+            {
+                add(new Etudiant(1,"sofien","Bensalem", Option.GAMIX,new Departement(),new ArrayList<Equipe>(),new ArrayList<Contrat>()));
+                add(new Etudiant(2,"soff","Bensalem", Option.GAMIX,new Departement(),new ArrayList<Equipe>(),new ArrayList<Contrat>()));
+            }
+        };
+        // Mock the behavior of the repository
+        Departement departement = new Departement(1,"TWIN",new ArrayList<Etudiant>());
+        Etudiant etudiant = new Etudiant(1,"sofien","Bensalem", Option.GAMIX,new Departement(),new ArrayList<Equipe>(),new ArrayList<Contrat>());
+        when(etudiantRepository.findById(1)).thenReturn(java.util.Optional.of(etudiant));
+        when(departementRepository.findById(1)).thenReturn(java.util.Optional.of(departement));
+
+        etudiantService.addEtudiant(etudiant);
+        departementService.addDepartement(departement);
+        etudiantRepository.save(etudiant);
+        departementRepository.save(departement);
+
+        // Invoke the service method
+        etudiantService.assignEtudiantToDepartement(1, 1);
+    }
+    @Test
+    public Etudiant testAddAndAssignEtudiantToEquipeAndContract() {
+        List<Etudiant> list= new ArrayList<Etudiant>() {
+            {
+                add(new Etudiant(1,"sofien","Bensalem", Option.GAMIX,new Departement(),new ArrayList<Equipe>(),new ArrayList<Contrat>()));
+                add(new Etudiant(2,"soff","Bensalem", Option.GAMIX,new Departement(),new ArrayList<Equipe>(),new ArrayList<Contrat>()));
+            }
+        };
+        List<Equipe> equipes = new ArrayList<Equipe>(){
+            {
+            add(new Equipe(1,"team",Niveau.EXPERT,list,new DetailEquipe()));
+            }
+        };
+        List<Contrat> contrats = new ArrayList<Contrat>(){
+            {
+                add(new Contrat(1,new Date(),new Date(),Specialite.IA,false,15,e));
+            }
+        };
+        // Mock the behavior of the repository
+        Etudiant e = new Etudiant(1,"sofien","Bensalem", Option.GAMIX,new Departement(),equipes,contrats);
+        Contrat contrat = new Contrat(1,new Date(),new Date(),Specialite.IA,false,15,e);
+        Equipe equipe = new Equipe(1,"team",Niveau.EXPERT,list,new DetailEquipe());
+        when(contratRepository.findById(1)).thenReturn(java.util.Optional.of(contrat));
+        when(equipeRepository.findById(1)).thenReturn(java.util.Optional.of(equipe));
+
+        etudiantService.addEtudiant(e);
+        etudiantRepository.save(e);
+        contratService.addContrat(contrat);
+        equipeService.addEquipe(equipe);
+        contratRepository.save(contrat);
+        equipeRepository.save(equipe);
+
+        // Invoke the service method
+        Etudiant result=etudiantService.addAndAssignEtudiantToEquipeAndContract(e, 1, 1);
+        verify(contratRepository, times(1)).findById(1);
+        verify(equipeRepository, times(1)).findById(1);
+        verify(etudiantRepository, times(1)).save(result);
+        return result;
     }
 
-    @Test
-    void findByDepartementIdDepartement() {
-    }
-
-    @Test
-    void findByEquipesNiveau() {
-    }
-
-    @Test
-    void retrieveEtudiantsByContratSpecialite() {
-    }
-
-    @Test
-    void retrieveEtudiantsByContratSpecialiteSQL() {
-    }
-
-    @Test
-    void addAndAssignEtudiantToEquipeAndContract() {
-    }
-
-    @Test
-    void getEtudiantsByDepartement() {
-    }
 }
+//
+//@SpringBootTest(classes = {KhaddemApplication.class})
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@ExtendWith(SpringExtension.class)
+//class EtudiantServiceImplJunitTest {
+//
+//    @Autowired
+//    EquipeRepository equipeRepository;
+//    @Autowired
+//    ContratRepository contratRepository;
+//    @Autowired
+//    EtudiantRepository etudiantRepository;
+//    @Autowired
+//    DepartementRepository departementRepository;
+//    @Autowired
+//    EtudiantServiceImpl etudiantService;
+//    @Autowired
+//    DepartementServiceImpl departementService;
+//    @Autowired
+//    ContratServiceImpl contratService;
+//    @Autowired
+//    EquipeServiceImpl equipeService;
+//
+//    Departement departement = new Departement(1,"TWIN",new ArrayList<Etudiant>());
+//    Equipe equipe = new Equipe(1,"team",Niveau.EXPERT,new ArrayList<Etudiant>(),new DetailEquipe());
+//    List<Equipe> equipes = new ArrayList<Equipe>(){
+//        {
+//            add(new Equipe(1,"team",Niveau.EXPERT,new ArrayList<Etudiant>(),new DetailEquipe()));
+//        }
+//    };
+//    List<Contrat> contrats = new ArrayList<Contrat>(){
+//        {
+//            add(new Contrat(1,new Date(),new Date(),Specialite.IA,false,15,e));
+//        }
+//    };
+//    Etudiant e = new Etudiant(1,"sofien","Bensalem", Option.GAMIX,departement,equipes,contrats);
+//    Contrat contrat = new Contrat(1,new Date(),new Date(),Specialite.IA,false,15,e);
+//
+//
+//
+//    @Test
+//    @Order(1)
+//    void testAddEtudiant(){
+//        equipeService.addEquipe(equipe);
+//        contratService.addContrat(contrat);
+//        departementService.addDepartement(departement);
+//        etudiantService.addEtudiant(e);
+//        equipeRepository.save(equipe);
+//        contratRepository.save(contrat);
+//        departementRepository.save(departement);
+//        etudiantRepository.save(e);
+//        Etudiant etudiant= etudiantService.addEtudiant(e);
+//        Assertions.assertEquals("sofien",etudiant.getNomE());
+//    }
+
+//    @Test
+//    @Order(2)
+//    void testRetrieveEtudiant(){
+//        Etudiant etudiant = etudiantService.retrieveEtudiant(1);
+//        Assertions.assertNotNull(etudiant);
+//    }
+
+
+
+
+//}
+
